8f41b16e94337fd57369f56e955057faa56202f5
sxiumz <164221063+sxiumz@users.noreply.github.com>
2025-12-04 02:11:56 +0200

    Add files via upload

---
+++ DeliverySystem/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DeliverySystem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+<img width="1917" height="1078" alt="Test photo" src="https://github.com/user-attachments/assets/6376cbe1-5255-4e19-82fe-81ee42881e31" />
